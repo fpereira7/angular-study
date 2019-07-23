f10a80989a4956e65560eb64777ced8ea11f82e3
Francisco <francisco.anpereira@gmail.com>
2019-07-22 22:13:05 -0300

    atualizacao parcial - status pessoa

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -11,9 +11,20 @@ export class ErrorHandlerService {
 
     if(typeof errorResponse === 'string') {
       msg = errorResponse;
+    } else if (errorResponse instanceof Response && errorResponse.status >= 400 && errorResponse.status <= 499){
+      let errors;
+      msg = 'Ocorreu um erro ao processar a sua solicitação';
+
+      try {
+        errors = errorResponse.json();
+
+        msg = errors[0].mensagemUsuario;
+      } catch (e) {}
+
+      console.error('Ocorreu um erro', errorResponse);
     } else {
       msg = 'Erro ao processar serviço remoto. Tente novamente.';
-      console.log('occorreu um erro', errorResponse);
+      console.error('Ocorreu um erro', errorResponse);
     }
 
     this.toasty.error(msg);
